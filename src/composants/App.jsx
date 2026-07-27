85467276305f0f2a8a1a9153f127847203d173b0
omar-ma66 <devcodefree66@gmail.com>
2026-07-27 10:17:00 +0200

    etude de react 2

---
+++ src/composants/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { people } from './data';
+import { getImageUrl } from './utils';
+import './App.css'
+
+
+export default function  App()
+{
+        const [count,setCount]=useState(0);    
+
+
+ const chemists = people.filter (person => person.profession === "chimiste");
+ const autre    = people.filter((person )=>
+  {
+    return person.profession !== "chimiste" ;
+  })
+  const listAutre = autre.map((person)=>{ 
+   return ( <li key={person.id}>
+    <img src={getImageUrl(person)}
+    alt={person.name} />
+    <p>
+        <b>{person.name}:</b>
+        {'' + person.profession +''}
+        celebre pour {person.accomplishement}
+      </p> 
+ </li>)
+  })
+ const listItems = chemists.map(person => 
+ <li key={person.id}>
+    <img src={getImageUrl(person)}
+    alt={person.name} />
+    <p>
+        <b>{person.name}:</b>
+        {'' + person.profession +''}
+        celebre pour {person.accomplishement}
+      </p> 
+ </li>)
+
+function HandleClick()
+{
+  const data = count + 1 ;
+
+              setCount(data);
+}
+
+  return (
+  <section>
+      <ul>{listItems}</ul> 
+  
+      <ul>{listAutre}</ul>
+  </section>
+        
+  );
+}
---
+++ src/composants/utils.jsx
@@ -0,0 +1,4 @@
+export function getImageUrl(person) {
+  // Exemple d'URL générée à partir de l'identifiant de la personne
+  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
+}
