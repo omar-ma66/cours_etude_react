787af4b37ce7cd7c1e83db5231a66f80b9fd5312
omar-ma66 <devcodefree66@gmail.com>
2026-07-31 15:31:41 +0200

    je continu

--- src/composants/App.jsx
+++ src/composants/App.jsx
@@ -1,23 +1,78 @@
-import { useState } from 'react';
-import Chat from './Chat.jsx';
-import ContactList from './ContactList.jsx';
+import { useReducer } from 'react';
+import AddTask from './AddTask.js';
+import TaskList from './TaskList.js';
 
-export default function Messenger() {
-  const [to, setTo] = useState(contacts[0]);
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    });
+  }
+
   return (
-    <div>
-      <ContactList
-        contacts={contacts}
-        selectedContact={to}
-        onSelect={contact => setTo(contact)}
+    <>
+      <h1>Voyage à Prague</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
       />
-      <Chat contact={to} />
-    </div>
-  )
+    </>
+  );
 }
 
-const contacts = [
-  { id: 0, name: 'Clara', email: 'clara@mail.com' },
-  { id: 1, name: 'Alice', email: 'alice@mail.com' },
-  { id: 2, name: 'Bob', email: 'bob@mail.com' }
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'changed': {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error('Action inconnue : ' + action.type);
+    }
+  }
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visiter le musée Franz-Kafka', done: true},
+  {id: 1, text: 'Voir un spectacle de marionnettes', done: false},
+  {id: 2, text: 'Prendre une photo du mur John Lennon', done: false},
 ];
